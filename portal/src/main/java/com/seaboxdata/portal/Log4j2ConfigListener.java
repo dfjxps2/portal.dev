959369ff655c3aa55732ccb5f73c16a9932b713e
Yu Xiao <xiaoy597@qq.com>
2018-08-23 18:45:45 +0800

    change uat configuration in pom.xml

--- portal/src/main/java/com/seaboxdata/portal/Log4j2ConfigListener.java
+++ portal/src/main/java/com/seaboxdata/portal/Log4j2ConfigListener.java
@@ -1,12 +1,18 @@
 package com.seaboxdata.portal;
 
-import java.util.Enumeration;
+import com.quick.portal.security.authority.metric.PropertiesUtil;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
-import org.apache.logging.log4j.core.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configurator;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
 
 
 public class Log4j2ConfigListener implements ServletContextListener {
@@ -14,6 +20,13 @@ public class Log4j2ConfigListener implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent arg0) {
+//        Path symLinkPath = Paths.get(arg0.getServletContext().getRealPath("/") + "/upload/files");
+//        try {
+//            Files.deleteIfExists(symLinkPath);
+//        }catch (IOException e){
+//            Logger logger = LoggerFactory.getLogger(getClass());
+//            logger.error("Failed to delete symbolic link for file.dir", e);
+//        }
     }
 
 
@@ -21,6 +34,20 @@ public class Log4j2ConfigListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent arg0) {
         String fileName = getContextParam(arg0);
         Configurator.initialize(null, fileName);
+
+//        Path realPath = Paths.get(PropertiesUtil.getPropery("file.dir"));
+//        Path symLinkPath = Paths.get(arg0.getServletContext().getRealPath("/") + "/upload/files");
+//        if (System.getProperty("os.name").equalsIgnoreCase("linux")) {
+//            try {
+//                System.out.println("Trying to create symbolic link from " + realPath.toString() + " to " + symLinkPath.toString());
+//                Files.createSymbolicLink(symLinkPath, realPath);
+//            } catch (FileAlreadyExistsException e) {
+//                // Do nothing ...
+//            } catch (Exception e) {
+//                Logger logger = LoggerFactory.getLogger(getClass());
+//                logger.error("Failed to create symbolic link for file.dir", e);
+//            }
+//        }
     }
 
 
